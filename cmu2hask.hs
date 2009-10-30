50d753368e1fe303939c21384b3eaf6505aa4a12
Noam <noam@duapov.(none)>
2009-10-30 02:23:43 +0200

    Changed to build Map at parse time

--- cmu2hask.hs
+++ cmu2hask.hs
@@ -1,3 +1,6 @@
+--- options to solve speed problem:
+--- * use CSV.hs which is by mmorow and very fast
+--- * use PerfectHash and Binary marshalling
 
 module Main where
 
@@ -10,52 +13,50 @@ import qualified Text.Parsec as P
 import qualified System
 
 import qualified Data.Char
+import qualified Data.Map as Map
 
 main :: IO ()
 main = do
   args <- System.getArgs
   let filename = args!!0
-  putStr "module ConvertedCMUDict where\n"
-  putStr "import qualified Data.Map\n"
-  putStr $ " -- Parsing " ++ show filename ++ "\n"
-  putStr "pronunciations = Data.Map.fromList ["
-  result <- (LP.parseFromFile convLines filename) 
+  result <- (LP.parseFromFile (convLines Map.empty) filename) 
   case result of
-    Left err -> putStr $ "Parse Error: " ++ show err ++ "\n"
-    Right bs -> putStr (unpackToString bs)
-  putStr "]\n"
+    Left err       -> putStr $ "Parse Error: " ++ show err ++ "\n"
+    Right pron_map -> case Map.lookup (args!!1) pron_map of 
+                        Nothing -> return ()
+                        Just pron -> putStr $ pron ++ "\n"
   return ()
 
 
+type MapSS = Map.Map String String
+
 -- todo why doesn't this keep running? just does one line...
-convLines :: LP.Parser LB.ByteString
-convLines = do converted <- P.many $ (P.try parseComment) P.<|> parseEntry
-               return $ LB.concat converted
+convLines :: MapSS -> LP.Parser MapSS
+convLines m = do converted <- P.many $ (P.try $ parseComment m) P.<|> (parseEntry m)
+                 return $ Map.unions converted
+                 
          
-test :: LP.Parser LB.ByteString   
-test = do a <- P.char 'A'
-          return $ packFromString "bla"
 
 unpackToString = map (Data.Char.chr . fromIntegral) . LB.unpack
 packFromString = LB.pack . (map $ fromIntegral . Data.Char.ord)
 
-parseComment :: LP.Parser LB.ByteString
-parseComment = do P.spaces
-                  P.string ";;;"
-                  rest <- P.many (P.noneOf "\n")
-                  P.newline
-                  return $ packFromString $ " -- " ++ rest ++ "\n"
+parseComment :: MapSS -> LP.Parser MapSS
+parseComment m = do P.spaces
+                    P.string ";;;"
+                    P.many (P.noneOf "\n")
+                    P.newline
+                    return m
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-parseEntry :: LP.Parser LB.ByteString
-parseEntry = do P.spaces
-                named_char <- P.optionMaybe . P.many1 . P.oneOf $ "-,:;!?/.'\"()&#%{}"
-                word <- P.many1  $ P.noneOf " "
-                P.spaces
-                pronunciation <- P.many $ P.noneOf "\n"
-                P.newline
-                return $ packFromString $ "   " ++ show (word, pronunciation) ++ ",\n"
+parseEntry :: MapSS -> LP.Parser MapSS
+parseEntry m = do P.spaces
+                  named_char <- P.optionMaybe . P.many1 . P.oneOf $ "-,:;!?/.'\"()&#%{}"
+                  word <- P.many1  $ P.noneOf " "
+                  P.spaces
+                  pronunciation <- P.many $ P.noneOf "\n"
+                  P.newline
+                  return $ (Map.insert word pronunciation m)
                 
 
 
