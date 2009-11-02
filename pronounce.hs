625b5e7e021f1e6a40dc85e5994e3c1c763d8534
Noam <noam@duapov.(none)>
2009-11-02 02:47:38 +0200

    Made prettier printing, and added multiple arg handling

--- pronounce.hs
+++ pronounce.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import qualified Data.Char as Char
+import qualified Data.List as List
 import qualified Data.Map as Map
 import qualified Data.Binary as Binary
 import qualified Data.ByteString as B
@@ -8,6 +10,13 @@ import qualified Data.ByteString.Char8 as BC
 
 import System
 
+upper = map Char.toUpper
+lower = map Char.toLower
+
+replace c d [] = []
+replace c d (x:xs) | c==x = d : (replace c d xs)
+replace c d (x:xs) = x : (replace c d xs)
+
 main :: IO ()
 main = do
   args <- getArgs
@@ -15,8 +24,10 @@ main = do
 --         putStr "Usage: pronounce <dictionary binary> <word to pronounce>"
 --         return 
   pronunciations <- fmap Binary.decode (LB.readFile (args!!0)) :: IO (Map.Map B.ByteString B.ByteString)
-  let pron = Map.lookup (BC.pack (args!!1)) pronunciations
-  case pron of
-    Nothing -> return ()
-    Just p -> putStr ((BC.unpack p) ++ "\n")
-  
+  let pron x = (Map.lookup (BC.pack (upper x)) pronunciations)
+      prons = map pron args
+      printEm Nothing = []
+      printEm (Just p) = (replace ' ' '-') . lower . BC.unpack $ p
+
+  putStr . concat . (List.intersperse " ") $ (map printEm prons)
+  putStr "\n"
